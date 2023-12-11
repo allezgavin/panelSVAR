e187f8bbcbd260d1f0924282c2a1b3684e128161
Gavin Xia <gx1@williams.edu>
2023-12-10 21:51:40 -0500

    citation added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+"""
+Calculates VAR, SVAR, and panel SVAR. Panel SVAR methods are from
+Pedroni, P. (2013), Econometrics, 1(2), 180-206; https://doi.org/10.3390/econometrics1020180
+
+Code still in development. Use for ECON 371 only (for now).
+Do NOT try to crash the code! I have not implemented much error handling.
+Incorrect input could lead to incorrect output.
+
+Your contribution is much appreciated!
+Please contact Gavin Xia at gx1@williams.edu for to make contributions or use the code for projects.
+"""
+
+
 from SVAR import *
 from panelSVAR import *
 
