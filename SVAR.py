Fixed bug in covariance mat in shortAndLong

SVAR.py
@@ -86,19 +86,22 @@ def SVAR(input): # possible mutation of input.df
 
     model = VAR(input.df)
     results = model.fit(maxlags=input.maxlags, ic=input.lagmethod)
-    # print(results.params)
+    # print(results.params) # VAR coefficients
+    # print(results.sigma_u) # Covariance matrix Omega_mu
     output.lag_order = results.k_ar
-    #print(input.df)
+    # print(input.df)
     prediction = model.predict(params=results.params, lags=output.lag_order)
     output.shock = input.df.iloc[output.lag_order:, :] - prediction # This step calculates mu. Will be tranformed into epsilon
     
-    # Calculate decomposition matrix M
+    # Estimate impulse response, without any transformation of the shocks (FACTOR = %identity(m))
     irf = results.irf(input.nsteps)
-    # Get numerical
+    # print(irf.irfs)
+
+    # Calculate decomposition matrix M
     F1 = np.zeros((input.size, input.size))
     for f in irf.irfs:
         F1 += f
-    M = shortAndLong(input.size, input.sr_constraint, input.lr_constraint, F1)
+    M = shortAndLong(results.sigma_u, input.sr_constraint, input.lr_constraint, F1)
     A1 = np.dot(F1, M)
     # Sign constraint
     signmat = np.identity(input.size)
@@ -111,6 +114,7 @@ def SVAR(input): # possible mutation of input.df
             if switch_sign:
                 signmat[j, j] = -1
     M = np.dot(M, signmat)
+    # print(M) # The M, or A0 matrix
 
     output.ir = irf.irfs
     for i in range(input.nsteps+1):

main.py
@@ -15,7 +15,10 @@ from SVAR import *
 from panelSVAR import *
 
 def run_panel():
-    plot = True # Does not plot anyways
+    """
+    EXAMPLE INPUT BELOW
+
+    plot = False
     savefig_path = ""
     excel_path = "pedroni_ppp.xls"
     excel_sheet_name = "Sheet1"
@@ -31,6 +34,32 @@ def run_panel():
     lr_constraint = [(1,2)]
     sr_sign = np.array([['+','.'],
                         ['.','+']])
+    lr_sign = np.array([['.','.'],
+                        ['.','.']])
+    maxlags = 4 # maximum lags to be considered for common shock responses
+    nsteps = 15   # desired number of steps for the impulse responses
+    lagmethod = 'aic'
+
+    bootstrap = False
+    ndraws = 2000
+    signif = 0.05 # significance level of bootstrap
+    """
+    plot = False
+    savefig_path = ""
+    excel_path = "test-run.xls"
+    excel_sheet_name = "Panel6_comm_all"
+    variables = {
+        # 1 for unit root, 0 for stationary
+        'CommodityIndex' : 1,
+        'Yreal' : 1,
+    }
+    shocks = ['Real', 'Nominal']
+    td_col = ["time"]
+    member_col = "country"
+    sr_constraint = []
+    lr_constraint = [(1,2)]
+    sr_sign = np.array([['+','+'],
+                        ['.','.']])
     lr_sign = np.array([['.','.'],
                         ['.','.']])
     maxlags = 4 # maximum lags to be considered for common shock responses
@@ -97,8 +126,8 @@ def run_var():
                         ['.','.']])
     lr_sign = np.array([['+','.'],
                         ['.','.']])
-    maxlags = 4 # maximum lags to be considered for common shock responses
-    nsteps = 15 # desired number of steps for the impulse responses
+    maxlags = 8 # maximum lags to be considered for common shock responses
+    nsteps = 40 # desired number of steps for the impulse responses
     lagmethod = 'aic'
 
     bootstrap = True
@@ -112,5 +141,5 @@ def run_var():
     SVAR(var_input)
 
 if __name__ == "__main__":
-    run_var()
-    # run_panel()
+    # run_var()
+    run_panel()

shortAndLong.py
@@ -1,12 +1,16 @@
 import numpy as np
 from scipy.optimize import minimize
 
-def shortAndLong(size, sr_constraint, lr_constraint, F1):
+def shortAndLong(Omega_mu, sr_constraint, lr_constraint, F1):
+    if np.array_equal(Omega_mu, Omega_mu.T):
+        size = Omega_mu.shape[0]
+    else:
+        raise ValueError("Covariance matrix must be symmetrical.")
+    
     M = np.identity(size) # initialize M
-    omega = np.identity(size)
 
     # Flatten M and omega for scipy.optimize
-    m = M.T.reshape(1,-1)[0]
+    m = M.flatten()
 
     # Get 1-indexed column of a flattened matrix.
     def get_col(x, col):
@@ -15,7 +19,7 @@ def shortAndLong(size, sr_constraint, lr_constraint, F1):
     def objective_func(x):
         # The Forbenius norm of (MM'-Omega)
         recovered = x.reshape((size, size)).T
-        return np.linalg.norm(np.dot(recovered, recovered.T)-omega, 'fro')
+        return np.linalg.norm(np.dot(recovered, recovered.T)-Omega_mu, 'fro')
 
     def sr_constraint_func(x, sr_cons):
         return x[size*(sr_cons[1]-1) + sr_cons[0]-1]
@@ -27,7 +31,7 @@ def shortAndLong(size, sr_constraint, lr_constraint, F1):
     for lr_cons in lr_constraint:
         cons.append({'type':'eq', 'fun':lr_constraint_func, 'args':(lr_cons,)})
     
-    result = minimize(objective_func, M, constraints = cons)
+    result = minimize(objective_func, m, constraints = cons)
     M = result.x.reshape((size, size)).T
     # print(M)
     # print(result.message)
@@ -35,11 +39,11 @@ def shortAndLong(size, sr_constraint, lr_constraint, F1):
     return M
 
 def test():
-    size = 3
-    F1 = np.array([[1,2,3],[4,5,6],[7,8,9]])
-    sr_constraint = [(1,1)]
-    lr_constraint = [(1,2), (3,1)]
-    shortAndLong(size, sr_constraint, lr_constraint, F1)
+    Omega_mu = np.array([[0.8,0.5],[0.5,0.8]])
+    F1 = np.array([[4,5],[7,8]])
+    sr_constraint = [(1,2)]
+    lr_constraint = []
+    shortAndLong(Omega_mu, sr_constraint, lr_constraint, F1)
 
 if __name__ == "__main__":
     test()